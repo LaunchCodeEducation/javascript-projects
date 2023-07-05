e0f20f8e2f658e13e85a2dbf970cce1960115e7e
John Woolbright <jwoolbright23@gmail.com>
2023-07-05 09:41:37 -0500

    added data-and-variables exercises and studios starter code

---
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -0,0 +1,11 @@
+// Declare and assign the variables below
+
+// Use console.log to print the 'typeof' each variable. Print one item per line.
+
+// Calculate a space mission below
+
+// Print the results of the space mission calculations below
+
+// Calculate a trip to the moon below
+
+// Print the results of the trip to the moon below
---
+++ data-and-variables/studios/data-and-variables-studio.js
@@ -0,0 +1,3 @@
+// Declare and Initialize Variables below
+
+// Display the LC04 Form to the console below using above variables
