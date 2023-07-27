2845d9e05f598d7f5307f353f253a506081b0e5d
John Woolbright <jwoolbright23@gmail.com>
2023-07-27 14:48:23 -0500

    booleans and conditionals exercises starter-code

---
+++ booleans-and-conditionals/part-2.js
@@ -0,0 +1,21 @@
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+// 3) Write conditional expressions to satisfy the following safety rules:
+
+// a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
+
+
+// b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+
+
+// c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+
+
+// 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
+
+console.log(/* "Yes" or "No" */);
---
+++ booleans-and-conditionals/part-3.js
@@ -0,0 +1,24 @@
+let engineIndicatorLight = 'red blinking';
+let fuelLevel = 21000;
+let engineTemperature = 1200;
+
+/* 5) Implement the following checks using if/else if/else statements:
+
+a) If fuelLevel is above 20000 AND engineTemperature is at or below 2500, print "Full tank. Engines good."
+
+b) If fuelLevel is above 10000 AND engineTemperature is at or below 2500, print "Fuel level above 50%.  Engines good."
+
+c) If fuelLevel is above 5000 AND engineTemperature is at or below 2500, print "Fuel level above 25%. Engines good."
+
+d) If fuelLevel is at or below 5000 OR engineTemperature is above 2500, print "Check fuel level. Engines running hot."
+
+e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndicatorLight is red blinking print "ENGINE FAILURE IMMINENT!" 
+
+f) Otherwise, print "Fuel and engine status pending..." */
+
+// Code 5a - 5f here:
+
+// 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
+
+/* 6) b) Code the following if/else check:
+If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
