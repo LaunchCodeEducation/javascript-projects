48c80a0466dd2af3868e22b7d2ed62817f2548d4
John Woolbright <jwoolbright23@gmail.com>
2023-07-27 16:59:09 -0500

    starter code for exercises and studio chapter 5

---
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -0,0 +1,15 @@
+// Initialize Variables below
+
+// add logic below to determine the amount of astronauts on the shuttle
+
+// add logic below to check if astronauts are ready
+
+// add logic below to check if the total mass is less than the maximum limit
+
+// add logic below to check if the fuel temp is no less than 200 OR no greater than 300
+
+// add logic below to determine if fuel level is appropriate
+
+// add logic below to check if weather conditions are appropriate
+
+// Based on the above conditions, verify that the shuttle is ready for LiftOff
