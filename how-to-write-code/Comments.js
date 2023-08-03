da74f6cf69d255b83c2510a0d9d71c4833322f83
John Woolbright <jwoolbright23@gmail.com>
2023-08-03 11:19:11 -0500

    added pre-work-starter-code

---
+++ how-to-write-code/Comments.js
@@ -0,0 +1,12 @@
+   // This demo shows off comments!
+
+   // console.log("This does not print.");
+
+   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+
+   /* Here is how
+   to have
+   multi-line
+   comments. */
+
+   console.log("Comments make your code more readable by others.");
