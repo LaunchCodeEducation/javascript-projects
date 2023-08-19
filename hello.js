30a51c7dc345071f6fa490db28e7f204bfe9d75b
stacijbrooks <142553873+stacijbrooks@users.noreply.github.com>
2023-08-19 17:47:06 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,22 @@
+/*  
+Welcome, new coder. 
+Enter your code on line 7 to print 'Hello, World!' 
+to the console (the panel on the right)
+*/
+
+console.log("Hello");
+
+/*  
+Next, PLAY! 
+Modify your code on line 7 to try to accomplish the tasks listed below:
+
+   1. Change the message that is printed.
+   2. Figure out what the parentheses do. Will the code work without them?
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   4. Remove the semi-colon, ;.
+   5. Print a number. (Bonus: Print two numbers added together).
+   6. Print multiple messages one after the other.
+   7. Print two messages on the same line.
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   9. Other. You choose!
+*/
