10ace97b662fd413aed784c13ceab4f423edc74a
John Woolbright <jwoolbright23@gmail.com>
2023-08-23 15:39:28 -0500

    added chapter example code for chapter 4, also added package.json for readline-sync

---
+++ data-and-variables/chapter-examples/critical-input-detail.js
@@ -0,0 +1,6 @@
+const input = require('readline-sync');
+
+let num1 = input.question("Enter a number: ");
+let num2 = input.question("Enter another number: ");
+
+console.log(num1 + num2);
---
+++ data-and-variables/chapter-examples/type-of.js
@@ -0,0 +1,3 @@
+console.log(typeof "Hello, World!");
+console.log(typeof 17);
+console.log(typeof 3.14);
