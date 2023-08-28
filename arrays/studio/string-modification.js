0251067a6097d3b706ee36cee264c1d6e2c07200
John Woolbright <jwoolbright23@gmail.com>
2023-08-28 14:02:09 -0500

    arrays-studio starter code complete

---
+++ arrays/studio/string-modification.js
@@ -0,0 +1,11 @@
+const input = require('readline-sync');
+let str = "LaunchCode";
+
+//1) Use string methods to remove the first three characters from the string and add them to the end.
+//Hint - define another variable to hold the new string or reassign the new string to str.
+
+//Use a template literal to print the original and modified string in a descriptive phrase.
+
+//2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
