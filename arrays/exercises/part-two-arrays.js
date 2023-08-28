ccb4e741c4b329ad5e82518bb818798a2573c7d4
John Woolbright <jwoolbright23@gmail.com>
2023-08-28 12:08:37 -0500

    array-exercises added

---
+++ arrays/exercises/part-two-arrays.js
@@ -0,0 +1,11 @@
+let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
+
+//1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
+
+//2) Remove the last item from the array with pop. Print the element removed and the updated array.
+
+//3) Remove the first item from the array with shift. Print the element removed and the updated array.
+
+//4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
+
+//5) Use a template literal to print the final array and its length.
