d494c44d1eff15d775653b33cc45356650b7f227
stacijbrooks <stacibrooksfitness@gmail.com>
2023-09-13 22:37:58 -0500

    Completed Booleans Part 2

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -7,15 +7,40 @@ let shuttleSpeed = 15000;
 
 // 3) Write conditional expressions to satisfy the following safety rules:
 
+  }
 // a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
-
+if (crewStatus) {
+    console.log("Crew Ready");
+  } else {
+    console.log("Crew Not Ready");
+if (computerStatusCode === 200) {
+    console.log("Please standy by. Computer is rebooting.");
+} else if (computerStatusCode === 400) {
+    console.log("Success! Computer online.");
+} else {
+    console.log("ALERT: Computer offline!");
+}
+}
+}
+}
 
 // b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
 
 
 // c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
-
+if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
 
 // 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
 
 console.log(/* "Yes" or "No" */);
+if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
+
+ */yes*\
