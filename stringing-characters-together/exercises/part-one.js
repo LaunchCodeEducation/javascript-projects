6738b1f02e7935e2d65f31db890131e95e532a02
Nagappa, Arpitha <arpithagn021@gmail.com>
2023-09-17 22:54:46 -0500

    Exercises

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,24 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+//console.log(String(num).length);
+console.log(num.toString().length);
+
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+//console.log(num.toString().split());
+//let num1=123.45;
+if (Number.isInteger(num)) {
+    console.log("0");
+ } else {
+    console.log(num.toString().split('.')[1].length);
+ }
+ 
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,11 +3,16 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log(language.slice(0,1)+language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+console.log();
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+let firstInitial = language[0];
+let lastInitial = language[4];
 
+let outputStr = `"The abbreviation for 'JavaScript' is" '${firstInitial}${lastInitial}'.`;
+
+console.log(outputStr);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
 //Part Three section Two
@@ -15,3 +20,4 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase.charAt(1).toUpperCase + notTitleCase.slice(2,5) + notTitleCase.charAt(7).toUpperCase + notTitleCase.slice(7,10) );
