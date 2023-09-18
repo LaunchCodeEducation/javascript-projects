7ee767fb7a2672c294dfb107ab986ec659b6ff36
Nagappa, Arpitha <arpithagn021@gmail.com>
2023-09-17 22:59:13 -0500

    Exercises

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,21 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+let newArray=[];
+newArray=holdCabinet1.concat(holdCabinet2);
+console.log(newArray);
+newArray=holdCabinet2.concat(holdCabinet1);
+console.log(newArray);
+console.log(holdCabinet1);
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.slice(2));
+console.log(holdCabinet1);//nope
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+console.log(holdCabinet1.reverse());
+console.log(holdCabinet1);
+console.log(holdCabinet2.sort());
+console.log(holdCabinet2);
+//Reverse will reverse the order of the array and it alters the original array. 
+//Sort will sort the elemnts in increasing order,sort will also alter the original array.
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,38 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
 
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
-
+let table =[];
+table.push(element1);
+table.push(element2);
+table.push(element26);
+console.log(table);
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+console.log(table[1], table[1][1]);//table[1] is one complete row in a array  and table[1][1] is representing element in an array.
 
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
-
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+let element3 = ['Lithium', 'Li', 6.941];
+let element5 = ['Boron', 'B', 10.811];
+let element6 = ['Carbon', 'C', 12.011];
+
+let table1 =[];
+table1.push(element3);
+table1.push(element5);
+table1.push(element6);
+console.log(table1);
+
+let table2=[];
+table2=[table,table1];
+console.log(table2);
+console.log(table2[1]);
+console.log(table2[1][0]);
+console.log(table2[1][0][0]);
