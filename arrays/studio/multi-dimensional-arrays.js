50abd4cde23be37977616e3726ea760e545a8117
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-09-19 00:10:20 -0500

    09.18.23 studio

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -4,11 +4,57 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+let newFood = food.split().sort();
+let newEquipment = equipment.split().sort();
+let newPets = pets.split().sort();
+let newSleepAids = sleepAids.split().sort();
+console.log(newFood, newEquipment, newPets, newSleepAids);
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [
+    [newEquipment],
+    [newFood],
+    [newPets],
+    [newSleepAids]
+]
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+let userInput = input.question("Please input a cabinet number (0-3): ");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+//tfw you didn't read the question
 
+switch (userInput ) {
+    case 0:
+        userInput === 0
+        console.log("Cabinet 0 contains: " + [food]);
+        break;
+    case 1:
+        userInput === 1
+        console.log("Cabinet 1 contains: " + [equipment]);
+        break;
+    case 2:
+        userInput === 2
+        console.log("Cabinet 2 contains: " + [pets]);
+        break;
+    case 3:
+        userInput === 3
+        console.log("Cabinet 3 contains: " + [sleepAids]);
+        break;
+    default:
+        console.log("Invalid cabinet number!");
+}
+
+/*if (userInput === 0) {
+    console.log(pets);
+} else if (userInput === 1) {
+    console.log(equipment);
+} else if (userInput === 2) {
+    console.log(food);
+} else if (userInput === 3) {
+    console.log(sleepAids);
+} else {
+    console.log("Invalid cabinet selected");
+};*/
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,18 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let newString = str.slice(3) + str.slice(0,3);
+console.log(newString)
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`Previously, the string was ${str}. Now it is ${newString}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let userInput = input.question("How many letters will be rearrange?");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if ( userInput === "three" || 3) {
+    console.log("Correct!");
+} else {
+    console.log("Your answer was longer than the correct response");
+};
