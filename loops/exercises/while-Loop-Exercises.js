03b1f21a5892af08c9d721a7479ffab36f8a736f
Nagappa, Arpitha <arpithagn021@gmail.com>
2023-09-21 15:28:01 -0500

    While-Loop

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,7 +1,7 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-let fuelLevel;
-let numOfAstronuts;
-let altitude;
+let fuelLevel=0;
+let numOfAstronauts=0;
+let altitude=0;
 
 
 /*Exercise #4: Construct while loops to do the following:
@@ -13,18 +13,18 @@ let altitude;
   }
  
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-while(numOfAstronuts>7)
+while(numOfAstronauts>1 || numOfAstronauts<7)
 {
- numOfAstronuts = input.question("Enter number of astronauts");
+ numOfAstronauts = input.question("Enter number of astronauts");
 }   
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
 
-  while (fuelLevel-100*numAstronauts >= 0) {
+  while (fuelLevel-100*numOfAstronauts >= 0) {
    altitude += 50;
-   fuelLevel -= 100*numAstronauts;
+   fuelLevel -= 100*numOfAstronauts;
    }
 
 
@@ -32,10 +32,12 @@ while(numOfAstronuts>7)
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
-let output = `The shuttle gained an altitude of ${altitude} km.`;
 
+let output = `The shuttle gained an altitude of ${altitude} km.`;
 if (altitude >= 2000) {
 output += " Orbit achieved!";
 }else {
   output+= "Failed to reach orbit."
 }
+ console.log(output);
+
