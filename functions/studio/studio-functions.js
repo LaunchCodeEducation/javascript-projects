a095a5af0ccbb6463fd61f37250e0dac48a1046c
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-09-25 13:44:03 -0500

    finished studio prep

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -9,6 +9,16 @@
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
+const reverseCharacters = function (thingToReverse) {
+  if (typeof thingToReverse === "string") {
+    return thingToReverse.split("").reverse().join("");
+  } else if (typeof thingToReverse === "number") {
+    return Number(reverseCharacters(thingToReverse.toString()));
+  }
+};
+const reversedString = reverseCharacters(1234);
+console.log(reversedString);
+
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
@@ -26,9 +36,25 @@
 // 5. Return the final, reversed array.
 // 6. Be sure to print the results from each test case in order to verify your code.
 
-let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
+const arrayReversal = function (anArray) {
+  let newArray = [];
+
+  for (let i = 0; i < anArray.length; i++) {
+    let reversedSet = reverseCharacters(anArray[i]);
+    newArray.push(reversedSet);
+  }
+  return newArray.reverse();
+};
+
+let arrayTest1 = ["apple", "potato", "Capitalized Words"];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
-let arrayTest3 = ['hello', 'world', 123, 'orange'];
+let arrayTest3 = ["hello", "world", 123, "orange"];
+
+console.log(arrayReversal(arrayTest1));
+
+console.log(arrayReversal(arrayTest2));
+
+console.log(arrayReversal(arrayTest3));
 
 // Bonus Missions
 
@@ -36,6 +62,26 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];
 // 2. Retrieve only the last character from strings with lengths of 3 or less.
 // 3. Retrieve only the first 3 characters from strings with lengths larger than 3.
 // 4. Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
+
+const funPhrase = function (aString) {
+  aNewArray = [];
+
+  if (aString.length < 4) {
+    aNewArray.push(aString[aString.length - 1]);
+  } else {
+    let tempArray = [];
+    for (let i = 0; i < 3; i++) {
+      tempArray.push(aString[i]);
+    }
+    aNewArray.push(tempArray.join("").toString());
+  }
+
+  return `We put the ${aNewArray.toString()} in ${aString}.`;
+};
+
+const testString = "Functions rock!";
+
+console.log(funPhrase(testString));
 
 // Test Function
 
@@ -49,3 +95,12 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];
 // 3. Call your area function by passing in two arguments - the length and width.
 // 4. If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
 // 5. Use a template literal to print, “The area is ____ cm^2.”
+
+const areaOfRectangle = function (length, width = length) {
+  return length * width;
+};
+
+const rectLength = 2;
+const rectWidth = 4;
+
+console.log(`The area is ${areaOfRectangle(rectLength)}`);
