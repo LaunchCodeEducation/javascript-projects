8d6943ed3eab69cd49df61a47a174eaa527ea919
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-10-01 18:22:07 -0500

    Added objects

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -5,11 +5,32 @@ let superChimpOne = {
    age: 6
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
    age: 5
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+};
+
+let tartigrade = {
+   name: "Almina",
+   species: "Axolotl Salamander",
+   mass: 0.0000000001,
+   age: 1
 };
 
 
