e40c6a8f077ad3e4f9e69dc6229341af4563b1ed
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-10-04 19:29:48 -0500

    Update randomSelect.js

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,6 @@
 function randomFromArray(arr){
-  //Your code here to select a random element from the array passed to the function.
-}
-
-//TODO: Export the randomFromArray function.
+  function randomFromArray(arr){
+    let index = Math.floor(Math.random()*arr.length);
+    return arr[index];
+ }
+}.
