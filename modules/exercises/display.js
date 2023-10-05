8a80160c014a53e496e9a690d9db42a396b43662
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-10-04 19:25:59 -0500

    Update display.js

--- modules/exercises/display.js
+++ modules/exercises/display.js
@@ -1,24 +1,3 @@
-//TODO: Export ONLY the printAll function.
-
-function printAll(names, tests, scores){
-  let header = 'Name';
-  let row = '';
-
-  for (let i = 0; i<tests.length; i++){
-    header += '\t'+tests[i];
-  }
-  console.log(header);
-
-  for (let name = 0; name<names.length; name++){
-    row = names[name];
-    for (let score = 0; score<scores[name].length;score++){
-      row += '\t'+scores[name][score];
-    }
-    console.log(row);
-  }
-  return;
-}
-
 function printStudentScores(index,students,tests,scores){
   console.log(`Test results for ${students[index]}:`);
   for (let i = 0; i<tests.length; i++){
@@ -34,3 +13,8 @@ function printTestScores(index,test,students,scores){
   }
   return;
 }
+
+const printAll = require('./display.js');
+
+
+
