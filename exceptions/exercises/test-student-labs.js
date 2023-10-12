55d6aae0b5f8656856cd74484eb336538a35b6aa
Arpitha Nagappa <arpithagn021@gmail.com>
2023-10-11 21:35:16 -0500

    Exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,15 @@
 function gradeLabs(labs) {
+  try{
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let student = lab.student;
+    console.log(`${student} code worked: ${result === 27}`);
+  }
+  }
+  catch(err)
+  {
+    console.log("Error thrown"); 
   }
 }
 
@@ -22,3 +29,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
