432b90e6895e71f43adb82204f6bbc9c0becbcbc
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-10-12 10:46:16 -0500

    exceptions/exercise/test-student-labs

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,8 +4,11 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
-}
-
+  if (runLab.hasOwnProperty(result));{
+  console.log(result);
+  } else {
+    let customError = new Error("Error thrown");
+  }
 let studentLabs = [
   {
     student: 'Carly',
@@ -22,3 +25,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
