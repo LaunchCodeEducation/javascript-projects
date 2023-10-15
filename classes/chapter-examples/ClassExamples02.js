83526ceea97ac9ab9ccfc35c74e11d3ca71ef3c7
Arpitha Nagappa <arpithagn021@gmail.com>
2023-10-15 16:49:32 -0500

    Classes exercise

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -14,4 +14,31 @@ let tortoise = new Astronaut('Speedy', 120);
 
 console.log(tortoise.name, tortoise.age, tortoise.mass);
 
+let tortoise1 = new Astronaut();
+
+console.log(tortoise1.name, tortoise1.age, tortoise1.mass);
+
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+
+let tortoise3 = new Astronaut('Speedy', 120, 100, 300);
+
+console.log(tortoise3);
+
+	
+class Car {
+  constructor(make, model, year, color, mpg){
+     this.make = make;
+     this.model = model;
+     this.year = year;
+     this.color = color;
+     this.mpg = mpg;
+  }
+}
+
+let myCar = new Car('Chevy', 'Astro', 1985, 'gray', 20);
+
+console.log(typeof myCar.year);
+
+let myCar1 = new Car('Tesla', 'Model S', 2019);
+
+console.log(myCar1);
--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,56 @@
 // Define your Book class here:
-
+class Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       this.title = title;
+       this.author = author;
+       this.copyright = copyright;
+       this.isbn = isbn;
+       this.pages = pages;
+       this.timesCheckedOut = timesCheckedOut;
+       this.discarded = discarded;
+    }
+ 
+    checkout(uses=1) {
+       this.timesCheckedOut += uses;
+    }
+ }
 
 // Define your Manual and Novel classes here:
+class Manual extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(currentYear){
+       if (currentYear-this.copyright > 5) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
+ 
+ class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(){
+       if (this.timesCheckedOut > 100) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
 
 
 // Declare the objects for exercises 2 and 3 here:
-
+let goodRead = new Manual('Pride and Prejudice', 'Jane Austen', 1813, '1111111111111', 432, 32, 'No');
+let makingTheShip = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
 
 // Code exercises 4 & 5 here:
+makingTheShip.dispose(2023);
+
+goodRead.checkout(5);
+goodRead.dispose();
+
+console.log(goodRead.timesCheckedOut);
+
+console.log(makingTheShip.discarded);
