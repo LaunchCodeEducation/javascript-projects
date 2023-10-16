7ce790f6f23804b0dfc0c6222dae4f281227d2f2
Arpitha Nagappa <arpithagn021@gmail.com>
2023-10-15 20:19:21 -0500

    Unit Testing - Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,43 @@
 
 let launchcode = {
-  
+    organization: 'nonprofit',
+    executiveDirector: 'Jeff',
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(i){
+        if(i%2==0 && i%3!=0 && i%5!=0)
+        {
+            return 'Launch!';
+        }
+        else if(i%2!=0 && i%3==0 && i%5!=0)
+        {
+            return 'Code!';
+        }
+        else if(i%2!=0 && i%3!=0 && i%5==0)
+        {
+            return 'Rocks!';
+        }
+        else if(i%2==0 && i%3==0 && i%5!=0)
+        {
+            return 'LaunchCode!';
+        }
+        else if(i%2!=0 && i%3==0 && i%5==0)
+        {
+            return 'Code Rocks!';
+        }
+        else if(i%2==0 && i%3!=0 && i%5==0)
+        {
+            return 'Launch Rocks! (CRASH!!!!)';
+        }
+        else if(i%2==0 && i%3==0 && i%5==0)
+        {
+            return 'LaunchCode Rocks!';
+        }
+        else if(i%2!=0 && i%3!=0 && i%5!=0)
+        {
+            return "Rutabagas! That doesn't work.";
+        }
+    }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,50 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+	it("launchcode contains the correct value for key organization", function() {
+		expect(launchcode).toHaveProperty("organization", "nonprofit");
+	});
+	it("launchcode contains the correct value for key executiveDirector", function() {
+		expect(launchcode).toHaveProperty("executiveDirector", "Jeff");
+	});
+	it("launchcode contains the correct value for key percentageCoolEmployees", function() {
+		expect(launchcode).toHaveProperty("percentageCoolEmployees", 100);
+	});
+	it("launchcode contains the correct value for array programsOffered", function() {
+		expect(launchcode.programsOffered).toEqual(expect.arrayContaining(["Web Development"]));
+	});
+	it("launchcode contains the correct value for array programsOffered", function() {
+		expect(launchcode.programsOffered).toEqual(expect.arrayContaining(["Data Analysis"]));
+	});
+	it("launchcode contains the correct value for array programsOffered", function() {
+		expect(launchcode.programsOffered).toEqual(expect.arrayContaining(["Liftoff"]));
+	});
+	it("launchcode contains the correct length for array programsOffered", function() {
+		expect(launchcode.programsOffered.length).toEqual(3);
+	});
+	it("launchOutput returns Launch! if number passed is only divisable by 2", function() {
+            expect(launchcode.launchOutput(4)).toEqual("Launch!");
+	});		
+	it("launchOutput returns Code! if number passed is only divisable by 3", function() {
+            expect(launchcode.launchOutput(9)).toEqual("Code!");
+	});	
+	it("launchOutput returns Rocks! if number passed is only divisable by 5", function() {
+            expect(launchcode.launchOutput(25)).toEqual("Rocks!");
+	});
+	it("launchOutput returns LaunchCode! if number passed is only divisable by 2 and 3", function() {
+            expect(launchcode.launchOutput(6)).toEqual("LaunchCode!");
+	});
+	it("launchOutput returns Code Rocks! if number passed is only divisable by 5 and 3", function() {
+            expect(launchcode.launchOutput(15)).toEqual("Code Rocks!");
+	});
+	it("launchOutput returns Launch Rocks! (CRASH!!!!) if number passed is only divisable by 5 and 2", function() {
+            expect(launchcode.launchOutput(10)).toEqual("Launch Rocks! (CRASH!!!!)");
+	});
+	it("launchOutput returns LaunchCode Rocks! if number passed is only divisable by 5, 3, and 2", function() {
+            expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!");
+	});
+	it("launchOutput returns Rutabagas! That doesn't work. if number passed is not divisable by 5, 3, or 2", function() {
+            expect(launchcode.launchOutput(7)).toEqual("Rutabagas! That doesn't work.");
+	});
   
 });
