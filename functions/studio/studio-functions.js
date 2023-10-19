009525b0bb185bf5e219e47c9e35997c621b187f
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-10-18 19:28:05 -0500

    Update studio-functions.js

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -5,6 +5,47 @@ function reverseCharacters(arr) = {   }
    let reversed = '' 
    for (i=-)
 
+function makeLine(size) {
+  let line = '';
+  for (let i = 0; i < size; i++) {
+    line += '#';
+  }
+  return line;
+}
+
+
+
+ function makeRectangle(width, height) {
+  let rectangle = '';
+  for (let i = 0; i < height; i++) {
+    rectangle += (makeLine(width) + '\n');
+  }
+  return rectangle.slice(0, -1);
+}       
+
+
+function makeDownwardStairs(height) {
+  let stairs = '';
+  for (let i = 0; i < height; i++) {
+    stairs += (makeLine(i+1) + '\n');
+  }
+  return stairs.slice(0, -1);
+} 
+
+
+
+
+function makeIsoscelesTriangle(height) {
+  let triangle = '';
+  for (let i = 0; i < height; i++) {
+    triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+  }
+  return triangle.slice(0, -1); 
+} 
+
+
+
+        
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
 // 2. Within the function, split the string into an array, then reverse the array.
 // 3. Use join to create the reversed string and return that string from the function.
