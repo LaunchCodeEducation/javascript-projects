5511ca3d60de2226464f88afd5b39124fbe982ee
camljess <147743168+camljess@users.noreply.github.com>
2023-10-26 17:21:03 -0400

    Worked on var.

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -11,11 +11,19 @@ console.log(typeof(225000000));
 console.log(typeof(38400));
 console.log(typeof(0.621));
 // Calculate a space mission below
-let milesToMars = kilometersToMars * milesPerKilometer;
-let hoursToMars = milesToMars / shuttleSpeedMph;
+let milesToMars = distanceToMarsKm * milesPerKm;
+console.log(milesToMars)
+let hoursToMars = milesToMars / shuttleSpeedMPH;
+console.log(hoursToMars)
 let daysToMars = hoursToMars / 24;
+console.log(daysToMars)
 // Print the results of the space mission calculations below
-
 // Calculate a trip to the moon below
-
+let milesToMoon = distanceToMoonKM * milesPerKm;
+console.log(milesToMoon);
+let hoursToMoon = milesToMoon / shuttleSpeedMPH;
+console.log(hoursToMoon);
+let dayToMoon = hoursToMoon /24;
 // Print the results of the trip to the moon below
+let shuttleName = "Determination"
+console.log(shuttleName + " will take " + dayToMoon + " days to reach the Moon. ")
