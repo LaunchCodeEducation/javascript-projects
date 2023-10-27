00ad08ff1229c7346636f75269fccfbfbaa2e927
camljess <147743168+camljess@users.noreply.github.com>
2023-10-27 16:25:40 -0400

    add

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,5 +1,20 @@
 // Initialize Variables below
-
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 6;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = "95%"
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
 // add logic below to verify all astronauts are ready
@@ -13,3 +28,32 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+if (astronautCount > 7) {
+    console.log("Too many Astronauts");
+    preparedForLiftOff = false;
+}
+if (astronautStatus != "ready") {
+    console.log("Astronaut Status NOT Ready");
+    preparedForLiftOff = false;
+}
+if (totalMassKg > maximumMassLimit) {
+    console.log("Too much weight");
+    preparedForLiftOff = false;
+}
+if (fuelTempCelsius < minimumFuelTemp || fuelTempCelsius > maximumFuelTemp) {
+    console.log("Temp outside acceptable limit");
+    preparedForLiftOff = false;
+}
+if (fuelLevel != "100%") {
+    console.log("Circle back to the gas station");
+    preparedForLiftOff = false;
+}
+if (weatherStatus != "clear") {
+    console.log("I can't see");
+    preparedForLiftOff = false;
+}
+if (!preparedForLiftOff) {
+    console.log("Not prepared for lift off");
+    }else{
+        console.log("Ready for Lift Off");
+    }
