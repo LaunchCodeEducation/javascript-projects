8aea94bb4118522320af3c5ffaa93fee6b172921
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-10-30 15:15:27 -0500

    STUDIO THE DOM AND EVENTS

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "red";
+     });
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
 }
-
 window.addEventListener("load", init);
+
+    console.log(window);
+    window.alert("Are you sure you want to abort the mission?")
+    let response = window.confirm("Mission aborted! Space shuttle returning home");
+    console.log(response);
+
+
+    
+
+
