02531a931e3c8499f36c65e14a7f1e6c5b113651
Arpitha Nagappa <arpithagn021@gmail.com>
2023-10-30 14:14:18 -0500

    DOM Events Studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,24 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function studio(){
+    const abortMission= document.getElementById("missionAbort");
+    const liftOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const shuttleHeight =document.getElementById("spaceShuttleHeight");
+
+    liftOff.addEventListener("click", event=>{
+        let takeoff = confirm("Confirm that the shuttle is ready for takeoff.");
+        if(takeoff== true){
+        flightStatus.innerHTML="Shuttle in flight.";
+        shuttleBackground.style.backgroundColor ="blue";
+        shuttleHeight.innerHTML=10000;
+        }
+        else{
+        let url =window.location.href;
+        window.open(url)
+    }
+ });
+
+}
+window.addEventListener("load", studio);
