c7a3f6a970478da2c1b096d2aa99c8f68ed72dcd
camljess <147743168+camljess@users.noreply.github.com>
2023-11-01 21:09:07 -0400

    add

--- loops/studio/index.js
+++ loops/studio/index.js
@@ -1,3 +1,4 @@
 const shuttleManagement = require('./solution.js');
-
+let = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans']
+let = 
 shuttleManagement.runProgram();
--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,20 +2,31 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
-  
+  let singleMeal = [];
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+for (let i = singleMeal; i < pantry.length; i++) {
+  (meals = pantry [i][5]);
+  console.log(meals);
+ 
+//create variable with empty array to represesnt single meal
 
+//loop through the array at index i 
+
+//push each item ito single meal array
+
+//outside of inner loop push single meal array into meals array
+}
 
   return meals;
 }
@@ -45,8 +56,8 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+ 
   
 
   /// TEST PART B HERE ///
