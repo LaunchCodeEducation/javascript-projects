c8ca9bdfbf7532004b17be8010a7c834d3dd2202
stacijbrooks <stacibrooksfitness@gmail.com>
2023-12-17 12:40:45 -0600

    Update consolelogexamples02.js

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,11 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+let day = "Thursday";
+console.log(day);
+
+day = "Friday";
+console.log(day);
+
+day = 21;
+console.log(day);
