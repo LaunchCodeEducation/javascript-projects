21a42365f9a363df70a5ed72f1e53e1a0933970c
Zachary L. Cain <zacharylcain@gmail.com>
2023-12-20 13:17:17 -0600

    Variables exercise

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,4 +1,9 @@
 // Declare and assign the variables below
+let shuttle = determination;
+let speed = 17500;
+let Distance to Mars = 225000000;
+let Distance to the Moon = 38400;
+let mpkh = .621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
