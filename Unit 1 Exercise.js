2d0ddee42b41287a92ee60ca8554537a1528784b
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-05 10:50:44 -0600

    Added more decriptions to the equation completed during the prework part of the course, ensured data integrity.

--- Unit 1 Exercise.js
+++ Unit 1 Exercise.js
@@ -1,26 +1,28 @@
-const nameOfShuttle = "determination";
-const shuttleSpeed = 17500;
-const marsDis = 225000000;
-const moonDis = 38400;
-const mpkh = .621;
-// Seconde set of instructions //
-console.log(typeof(nameOfShuttle));
-console.log(typeof(shuttleSpeed));
-console.log(typeof(marsDis));
-console.log(typeof(moonDis));
-console.log(typeof(mpkh));
-// Third set of instructions //
-console.log(marsDis * mpkh); // How many Miles to Mars? //
+// First we want to asign the variables their name // 
+let nameOfShuttle = "determination";
+let shuttleSpeed = 17500;
+let marsDis = 225000000;
+let moonDis = 38400;
+let mpkh = .621;
+// Seconde set of instructions we need to identify the data type of the variable we are re-asigning //
+console.log(typeof(nameOfShuttle)); // string //
+console.log(typeof(shuttleSpeed)); // number //
+console.log(typeof(marsDis)); // number //
+console.log(typeof(moonDis)); // number //
+console.log(typeof(mpkh)); // number // 
+// As we can see after running and debuging we get: string, number, number, number, number; // 
+// Third set of instructions we need to write the equation to identify the result of our problem we are trying to resolve//
+console.log(marsDis * mpkh); // How many Miles to Mars in mpkh? //
 console.log(marsDis / shuttleSpeed); // Hours it will take to get to Mars in hours //
 let hoursToMars = marsDis / shuttleSpeed; // Assignment of the solution to define hoursToMars //
 let daysToMars =  hoursToMars / 24; // Assignment of daysToMars after solution //
-console.log(hoursToMars);
+console.log(hoursToMars); // Testing first set of variable instructions to solve for hoursToMars and daysToMars // 
 console.log(daysToMars);
-// Time to print the solution //
-console.log(nameOfShuttle,"will take",daysToMars,"to reach mars"); // Solution to exersize UwU //
-// Equation challenge for Moon //
-console.log(moonDis * mpkh);
+// Time to print the solution hooray!!!//
+console.log(nameOfShuttle,"will take",daysToMars,"to reach mars"); // Solution to exersize //
+// Equation challenge for Moon, Part 2 //
+console.log(moonDis * mpkh);// testing the variables on the console for correct result before final equation //
 console.log(moonDis/shuttleSpeed);
-let hoursToMoon = moonDis/shuttleSpeed;
+let hoursToMoon = moonDis/shuttleSpeed; // asignment of variables for equation //
 let daysToMoon = hoursToMoon / 24;
-console.log(nameOfShuttle, "will take",daysToMoon, "to reach the Moon");
+console.log(nameOfShuttle, "will take",daysToMoon, "to reach the Moon"); // final part of the equation printed on console //
