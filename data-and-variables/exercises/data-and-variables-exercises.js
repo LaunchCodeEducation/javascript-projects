4d9dd8f98c5695232c60f81a659a1193f7bbc323
wisevaishu <vpm.win@gmail.com>
2024-01-08 15:26:18 -0600

    Data and Variables Exercise done

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,26 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof shuttleName);
+
+let milesToMars = distanceToMarsKm * 0.621;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = Math.round(hoursToMars / 24);
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars");
+
+let milesToMoon = distanceToMoonKm * 0.621;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon =hoursToMoon / 24;
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
