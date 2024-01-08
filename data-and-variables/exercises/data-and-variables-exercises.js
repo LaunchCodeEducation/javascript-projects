92ca6c4343ba37b042436a7ed27893f6da408196
fowlerrc <142541725+fowlerrc@users.noreply.github.com>
2024-01-07 21:15:24 -0600

    Update data-and-variables-exercises.js

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,37 @@
 // Declare and assign the variables below
-
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let kilometersToMars = 225000000;
+let kilometersToMoon = 38400
+const milesPerKilometer = 0.621;
+let milesToMars = 'kilometersToMars * milesPerKilometer';
+let hoursToMars = 'milesToMars / shuttleSpeedMph';
+let daysToMars = 'hoursToMars / 24';
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof 'shuttleName');
+string
+ console.log(typeof 'shuttleSpeedMph');
+string
+console.log(typeof 'kilometersToMars');
+string
+ console.log(typeof 'kilometersToMoon');
+string
+console.log(typeof 'milesPerKilometer');
+string
+console.log(typeof 'milesToMars');
+string
+console.log(typeof 'hoursToMars');
+string
+console.log(typeof 'daysToMars');
+string
 // Calculate a space mission below
-
+console.log(daysToMars)
+332.67857142857144
 // Print the results of the space mission calculations below
-
+let milesToMoon = 'kilometersToMoon * milesPerKilometer'
+let hoursToMoon = 'milesToMoon / shuttleSpeedMph'
+let daysToMoon = 'hoursToMoon / 24'
 // Calculate a trip to the moon below
-
+ console.log(daysToMoon)
+0.05677714285714286
 // Print the results of the trip to the moon below
