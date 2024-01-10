e95fa3f4a20850ba7fadc988198949ea829131bc
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-10 12:50:51 -0600

    all deguging exersices were done in errors2

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -6,28 +6,29 @@
 
 let launchReady = false;
 let fuelLevel = 17000;
-// let crewStatus = true;
-// let computerStatus = 'green';
+let crewStatus =true;
+let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
-   console.log('Fuel level cleared.');
-   launchReady = true;
+   console.log('Fuel level cleared.')
+   launchReady === true;
 } else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+   console.log('WARNING: Insufficient fuel!')
+   launchReady === false;
 }
 
-// if (crewStatus && computerStatus === 'green'){
-//    console.log('Crew & computer cleared.');
-//    launchReady = true;
-// } else {
-//    console.log('WARNING: Crew or computer not ready!');
-//    launchReady = false;
-// }
+ if (crewStatus && computerStatus === 'green'){
+    console.log('Crew & computer cleared.');
+    launchReady === true;
+ } else {
+    console.log('WARNING: Crew or computer not ready!');
+    launchReady === false;
+ }
 
-// if (launchReady) {
-//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
-//    console.log('Liftoff!');
-// } else {
-//    console.log('Launch scrubbed.');
-// }
+ if (launchReady === true) {
+    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+    console.log('Liftoff!');
+ } else {
+    console.log('Launch scrubbed.');
+ }
+ // Launch control was sent the incorect message due to the syntax error of = instead of using === // 
---
+++ t2.js
@@ -0,0 +1,5 @@
+console.log(typeof("Hello World"));
+console.log(Number("23"));
+let car = "Volvo";
+console.log(car);
+console.log("")
