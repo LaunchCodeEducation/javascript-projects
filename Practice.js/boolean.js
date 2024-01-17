32fdab743aa6306457c237258f59b1160c06cf58
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-17 13:00:58 -0600

    additional changes to assisst with practice.

---
+++ Practice.js/boolean.js
@@ -0,0 +1,14 @@
+// Exercise 1 for "Advanced Strings" //
+
+/* if (String(num).includes('.'))
+{console.log(string(num).length - 1 );}
+else {
+    console.log(string(num).length);
+} */
+
+// Exercise 2 for  "Advanced strings //
+
+let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT ";
+let dna2 = dna.toUpperCase().dna.trim(); 
+console.log(dna);
+
