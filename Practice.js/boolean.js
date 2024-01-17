0b6ae9d5b14e1d674a9a94cbd4133a1ac06f998a
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-17 14:56:04 -0600

    additional changes for the exersices

--- Practice.js/boolean.js
+++ Practice.js/boolean.js
@@ -1,14 +1,22 @@
 // Exercise 1 for "Advanced Strings" //
-
-/* if (String(num).includes('.'))
-{console.log(string(num).length - 1 );}
-else {
+/* let num = 123.45;
+if (String(num).includes('.')){
+    console.log(string(num).length - 1);
+} else {
     console.log(string(num).length);
 } */
 
 // Exercise 2 for  "Advanced strings //
 
-let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT ";
-let dna2 = dna.toUpperCase().dna.trim(); 
+let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    "; 
+
+dna = dna.trim().toUpperCase().replace('GCT','AGG');
 console.log(dna);
+
+
+
+
+
+
+
 
