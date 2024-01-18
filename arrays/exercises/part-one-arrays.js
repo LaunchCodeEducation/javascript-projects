c201b110173c35ea7d5f3d279315d13d6e0b2976
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-17 22:38:57 -0600

    changes to more of the array practice questions

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,9 @@
 //Create an array called practiceFile with the following entry: 273.15
-
+let praccticeFile =[273.15];
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
+praccticeFile[1] = 42;
+praccticeFile[2] = "hello";
+console.log(praccticeFile);
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+praccticeFile.push(-4.6,87);
+console.log(praccticeFile);
