272c329f9eb404bf17891c7c002807b68c7e86fe
cain9209 <zacharylcain@gmail.com>
2024-01-18 20:24:59 -0600

    changes to string modification

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,10 +3,10 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let str2 = str.replace('L',' ').replace('a',' ').replace('u',' ');
+let str2 = str.slice(0,3).concat("Lau");
 console.log(str2);
 //Use a template literal to print the original and modified string in a descriptive phrase.
-console.log(`${str}\n${str2}`);
+
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 let answer = ' '; 
 let question = 'Enter the number of letters that will be relocated.'
@@ -14,3 +14,29 @@ function askQuestion() {
 answer = input.question(question);
 } 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+{
+    let strInput = '';
+    let strStart = '';
+    let strCalc = '';
+    let strEnd = '';
+    let strFinal = '';
+
+    strInput = input.question("Enter a number from 1-9: ");
+    strInput = Number(strInput)
+
+    if (strInput > 9 || strInput < 1){
+        console.log('Input outside of range, defaulting to "3"')
+        strInput = 3
+        strStart = str.slice(0, strInput);
+        strCalc = strStart.length - str.length;
+        strEnd = str.slice(strCalc);
+        strFinal = strEnd.concat(strStart);
+        console.log(`LaunchCode? Moar like ${strFinal} amirite? hehe.heh.`)
+    } else {
+        strStart = str.slice(0, strInput);
+        strCalc = strStart.length - str.length;
+        strEnd = str.slice(strCalc);
+        strFinal = strEnd.concat(strStart);
+        console.log(`LaunchCode? Moar like ${strFinal} amirite? hehe.heh.`)
+    };
+}
