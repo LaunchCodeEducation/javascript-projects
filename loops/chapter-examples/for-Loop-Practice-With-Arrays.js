de97f96eae3aadb3f3594296cb78dca36e2e7a85
cain9209 <zacharylcain@gmail.com>
2024-01-24 20:18:23 -0600

    addition changes to loops

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,12 @@
 // create an array variable containing the names
 
 // write a for loop that prints each name on a different line
+for(i=0;i<20;i++){
+    console.log(i);
+}
+for(i=3;i<29;i+=2){
+    console.log(i);
+}
+for(i = 12;i > -14; i-=2){
+    console.log(i);
+}
