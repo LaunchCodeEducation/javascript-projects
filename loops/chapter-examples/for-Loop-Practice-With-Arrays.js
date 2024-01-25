154926036559840b5374006164844fe5c15066f9
cain9209 <zacharylcain@gmail.com>
2024-01-24 20:59:56 -0600

    changes to loopps

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -10,3 +10,16 @@ for(i=3;i<29;i+=2){
 for(i = 12;i > -14; i-=2){
     console.log(i);
 }
+// Par two //
+
+let lcArr = [1, 5, 'LC101', 'blue', 42];
+ for(i=0;i<5;i++){
+console.log(lcArr[i]);
+}
+let course = "LaunchCode";
+let reversedCourse = [];
+for(i = course.length - 1; i < course.length;i++){
+   
+ reversedCourse +=  reversedCourse + course[i];
+}
+console.log(reversedCourse);
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,10 @@
 // Create a string variable containing your name.
-
-
+let myName = ["Zack"];
+let reversedName = [''];
 // Write a for loop that prints each character in your name on a different line.
+for(let i=0;i<myName.length;i++){
+    reversedName = reversedName[i] + myName;
+}
+console.log(reversedName);
+
+
