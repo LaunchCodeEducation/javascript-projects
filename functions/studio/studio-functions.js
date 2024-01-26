e1244ccdb345342256e5539a7721cecf72fdd5bb
cain9209 <zacharylcain@gmail.com>
2024-01-25 19:37:21 -0600

    studios

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -16,16 +16,21 @@ console.log(reversedArr);
 
 // Part Two: Reverse Digits
 let practiceArr = [1234,'LC101',8675309,'radar'];
-function reverseCharacters(stringToReverse){
-    let reversedArr = [];
-for(let i = 0; i < stringToReverse.length;i++){
-reversedArr = stringToReverse[i].split('').reverse().join('')
-}
-}
-   
+let newArray = [];
 // 2. If typeof is ‘string’, return the reversed string as before.
+function reverseCharacters(reversedString2){
+   for(let i = 0;i < reversedString2.length;i++)
+   if(reversedString2[i] === String){
+    newArray.push(reversedString2[i].split('').reverse().join(''))
+   } else if(reversedString2[i] === Number)
+   newArray.push(reversedString2[i].split('').reverse().join('').string()) 
+}
+return newArray;
+
+console.log(newArray);
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
+   
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
 
 // Part Three: Complete Reversal
@@ -36,10 +41,10 @@ reversedArr = stringToReverse[i].split('').reverse().join('')
 // 4. Add the reversed string (or number) to the array defined in part ‘a’.
 // 5. Return the final, reversed array.
 // 6. Be sure to print the results from each test case in order to verify your code.
-/*
+
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
-let arrayTest3 = ['hello', 'world', 123, 'orange'];*/
+let arrayTest3 = ['hello', 'world', 123, 'orange'];
 
 // Bonus Missions
 
@@ -60,3 +65,4 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];*/
 // 3. Call your area function by passing in two arguments - the length and width.
 // 4. If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
 // 5. Use a template literal to print, “The area is ____ cm^2.”
+
