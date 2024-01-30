d9bda674afa5a374b18465165461050a0352812e
wisevaishu <vpm.win@gmail.com>
2024-01-29 20:25:24 -0600

    More On Functions Studio Done

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,10 +1,29 @@
-//1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
+//1) Create a function with an array of numbers as its parameter. 
+//The function should iterate through the array and return the minimum value from the array. 
+//Hint: Use what you know about if statements to identify and store the smallest value within 
+//the array.
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-//Using one of the test arrays as the argument, call your function inside the console.log statement below.
+//Using one of the test arrays as the argument, call your function inside the console.
+//log statement below.
 
-console.log(/* your code here */);
+function minNumberInArray (arr)
+{
+    let minValue=arr[0];
+    for (let item of arr)   
+    {
+        if (item<minValue)
+        {
+            minValue=item;
+        }
+    }
+    return minValue;
+}
+
+console.log(minNumberInArray(nums1));
+console.log(minNumberInArray(nums2));
+console.log(minNumberInArray(nums3));
