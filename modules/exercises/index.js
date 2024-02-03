15ce0b9b9b8ab6e7f662abfcadc27a98d47b8f46
Radha Sri Vepuri <143245819+radha-joys@users.noreply.github.com>
2024-02-03 10:30:17 +0000

    Class 8 Exercise

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -1,8 +1,8 @@
 //Import modules:
-const input = //Import readline-sync.
-const averages = //Import functions from averages.js.
-const printAll = //Import function from display.js.
-const randomSelect = //Import function from randomSelect.js.
+const input = require('readline-sync');//Import readline-sync.
+const averages = require('./ScoreCalcs/averages.js');//Import functions from averages.js.
+const printAll = require('./display.js');//Import function from display.js.
+const randomSelect = require('./randomSelect.js');//Import function from randomSelect.js.
 
 //Candidate data:
 let astronauts = ['Fox','Turtle','Cat','Hippo','Dog'];
@@ -19,18 +19,19 @@ for (let i = 0; i<prompts.length; i++){
   if (response.toLowerCase()==='y'){
     if (i===0){
       //Call 'printAll' here and pass in all necessary arguments.
+      printAll.printAll(astronauts, testTitles, scores);
     } else if (i===1){
       for (let j = 0; j<testTitles.length; j++){
-        let avg = //Call 'averageForTest' here. Pass in j and scores as arguments.
+        let avg = averages.averageForTest(j, scores);//Call 'averageForTest' here. Pass in j and scores as arguments.
         console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i===2){
       for (let j = 0; j<astronauts.length; j++){
-        let avg = //Call 'averageForStudent' here. Pass in j and scores as arguments.
+        let avg = averages.averageForStudent(j, scores);//Call 'averageForStudent' here. Pass in j and scores as arguments.
         console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = //Call 'randomSelect' to pick a spacewalker from the astronauts array.
+      let walker = randomSelect.randomFromArray(astronauts);//Call 'randomSelect' to pick a spacewalker from the astronauts array.
       console.log(`${walker} is the next spacewalker.`);
     }
   } else {
--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,9 @@
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  return arr[Math.floor(Math.random()*arr.length)];
 }
 
 //TODO: Export the randomFromArray function.
+module.exports = {
+  randomFromArray: randomFromArray
+}
