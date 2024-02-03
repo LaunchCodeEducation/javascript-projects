ae5e8437a342c5ae906eb7b48aa0f7c942839028
Radha Sri Vepuri <143245819+radha-joys@users.noreply.github.com>
2024-02-03 10:11:19 +0000

    Code Update

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -7,3 +7,6 @@ let tortoiseOne = {
 };
 
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+for(item in tortoiseOne){
+   console.log(item + " : " + tortoiseOne[item]);
+}
