2ebdffd82947fbbf680126ac063cf37b5c6bb679
wisevaishu <vpm.win@gmail.com>
2024-02-08 20:43:37 -0600

    unit testing studio done

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,69 @@
 
+
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(number) {
+        let no = 0;
+        no = number;
+       
+        if (no%2===0 || no%3===0 || no%5===0)
+        {
+            if (no%2===0 && no%3===0 && no%5===0)
+                return 'LaunchCode Rocks!';
+            else if (no%3===0 && no%5===0)
+                return 'Code Rocks!';
+            else if (no%2===0 && no%3===0)
+                return 'LaunchCode!';
+            else if (no%2===0 && no%5===0)
+                return 'Launch Rocks!';
+            else if (no%3===0)
+                return 'Code!';
+            else if (no%2===0)
+                return 'Launch!';   
+            else if (no%5===0)
+                return 'Rocks!';
+        }   
+        else 
+            return 'Rutabagas! That doesnt work.';
+    }
 }
 
+function temp(number)
+{
+    let returnStrings = {
+    2 : "Launch!",
+    3 : "Code!",
+    5 : "Rocks!"
+    }
+    
+    let outputString = '';
+    let numArr = [];
+    for(key in returnStrings)
+    {
+        if (number % key == 0)
+        {
+            numArr.push(Number(key));
+            outputString += returnStrings[key];
+        }
+    }
+    if ( numArr[0]==2 && numArr[1]==5 )
+        outputString = 'Launch Rocks!';
+    if ( numArr[0]==3 && numArr[1]==5 )
+        outputString = 'Code Rocks!';
+    if (numArr.length===3)
+        outputString = 'LaunchCode Rocks!';
+    console.log(numArr);
+    return outputString;
+}
+
+//console.log(temp(10));
+console.log(temp(30));
+
+
 module.exports = launchcode;
+
+  
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,57 @@
 // launchcode.test.js code:
+
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("value of organization is nonprofit", function(){
+    expect(launchcode.organization).toBe("nonprofit");
+ });
+
+ test("executiveDirector is Jeff", function(){
+  expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("percentageCoolEmployees is 100", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+    });
+
+    test("programsOffered should be Web Development, Data Analysis, Liftoff", function(){
+      expect(launchcode.programsOffered[2]).toContain("Liftoff", "Data Analysis", "Web Development");
+      expect(launchcode.programsOffered.length).toBe(3);
+      });
+
+      test("When passed a number that is ONLY divisible by 2, return 'Launch!'", function(){
+        expect(launchcode.launchOutput(2)).toBe("Launch!");
+      });
+
+      test("When passed a number that is divisible by 2 and 5, return 'Launch Rocks!'", function(){
+        expect(launchcode.launchOutput(10)).toBe("Launch Rocks!");
+      });
+
+      test("When passed a number that is ONLY divisible by 5, return 'Rocks!'", function(){
+        expect(launchcode.launchOutput(5)).toBe("Rocks!");
+      });
+
+      test("When passed a number that is divisible by 2,3 & 5 return 'LaunchCode Rocks!'", function(){
+        expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+      });
+
+      test("When passed a number that is divisible by 2 and 3, return 'LaunchCode!'", function(){
+        expect(launchcode.launchOutput(36)).toBe("LaunchCode!");
+      });
+
+      test("When passed a number that is divisible by 3 and 5, return 'Code Rocks!'", function(){
+        expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+      });
+
+      test("When passed a number that is ONLY divisible by 3, return 'Code!'", function(){
+        expect(launchcode.launchOutput(3)).toBe("Code!");
+      });
+
+      test("When passed a number that is NOT divisible by 2,3 & 5 return 'Rutabagas! That doesnt work.'", function(){
+        expect(launchcode.launchOutput(11)).toBe("Rutabagas! That doesnt work.");
+      });
+
 });
