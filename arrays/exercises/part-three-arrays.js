afb65ee08941dbefbb4c308de6a9b9b9c8c9be3c
nsirtak <nsirtak@gmail.com>
2024-05-08 15:21:26 -0500

    arrays

--- arrays/exercises/part-three-arrays.js
+++ arrays/exercises/part-three-arrays.js
@@ -3,7 +3,11 @@ let cargoHold = [1138, 'space suits', 'parrot', 'instruction manual', 'meal pack
 //Use splice to make the following changes to the cargoHold array. Be sure to print the array after each step to confirm your updates.
 
 //1) Insert the string 'keys' at index 3 without replacing any other entries.
-
+cargoHold.splice(3,0,"keys");
+console.log(cargoHold);
 //2) Remove ‘instruction manual’ from the array. (Hint: indexOf is helpful to avoid manually counting an index).
-
+cargoHold.splice(4,1);
+console.log(cargoHold);
 //3) Replace the elements at indexes 2 - 4 with the items ‘cat’, ‘fob’, and ‘string cheese’.
+cargoHold.splice(2,3,"cat","fob","string cheese");
+console.log(cargoHold);
