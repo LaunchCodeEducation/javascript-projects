5bdc6f49d775acdc3897d9f047229e35d2d11c80
Felicia <classic_felicia@hotmail.com>
2024-05-10 10:15:08 -0500

    part3-5 updated

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,7 +2,12 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split('',10));
+console.log(str.split(' '));
+// string.split(separator, limit)
+// separator	Optional.
+// A string or regular expression to use for splitting.
+// If omitted, an array with the original string is returned.
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
 
 //3) Do split or join change the original string/array?
