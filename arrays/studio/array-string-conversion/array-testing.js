1aa1fa40218257678533dabd6790f86a51ee4c5d
Felicia <classic_felicia@hotmail.com>
2024-05-10 22:07:30 -0500

    update array-testing and string mod

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -1,48 +1,54 @@
 let protoArray1 = "3,6,9,12";
-let protoArray2 = "A;C;M;E";
-let protoArray3 = "space delimited string";
+let protoArray2 = "M;E;C;A";
+let protoArray3 = "delimited space string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
-function reverseCommas(commaString) {
+function reverseCommas(checkCommaString) {
 	//TODO: 1. create and initiate your variables.
-	let check = commaString
+	let check = checkCommaString.includes(",");
 	let output;
 	//TODO: 2. write the code required for this step
-	if (check.includes(",")) {
-		output = check.split(",").reverse().join(",")
+	if (check) {
+		output = checkCommaString.split(",").reverse().join(",");
 	}
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
-function semiDash() {
-	let check;
+function semiDash(checkSemiString) {
+	let check = checkSemiString.includes(";");
 	let output;
-//TODO: write the code required for this step
+	//TODO: write the code required for this step
+	if (check) {
+		output = checkSemiString.split(";").reverse().join("-");
+	}
 
-  
 	return output;
 }
 
 //4)
-function reverseSpaces() {
-	let check;
+function reverseSpaces(checkSpaceString) {
+	let check = checkSpaceString.includes(" ");
 	let output;
-  //TODO: write the code required for this step
-
+	//TODO: write the code required for this step
+	if (check) {
+		output = checkSpaceString.split(" ").reverse().join(" ");
+	}
 	return output;
 }
 
 //5)
-function commaSpace() {
-	let check;
+function commaSpace(checkcommaSpace) {
+	let check = checkcommaSpace.includes(", ");
 	let output;
 	//TODO: write the code required for this step
-  
+	if (check) {
+		output = checkcommaSpace.split(", ").reverse().join(",")
+	}
 	return output;
 }
 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,22 +3,22 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-console.log(str.slice(3,10));
-let newStr = (str.slice(3,10) + str.slice(0,3));
+console.log(str.slice(3,str.length));
+let newStr = (str.slice(3,str.length) + str.slice(0,3));
 console.log(newStr);
 //Use a template literal to print the original and modified string in a descriptive phrase.
 console.log(`This is the original string "${str}", and this is the modified string "${newStr}".`);
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 let userinput = input.question("Enter a number to reloate from: ");
-userChoice = (str.slice(userinput,10)) + str.slice(0,userinput);
+let userChoice = (str.slice(userinput,str.length)) + str.slice(0,userinput);
 console.log(userChoice);
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. 
 // Also, the template literal should note the error.
-length = console.log(str.length);
-if (userinput > 10) {
+let length = (str.length);
+if (userinput > length) {
     console.log(newStr)
-    console.log("This words is only `${length}` letters long, please try again!");
+    console.log(`This words is only ${length} letters long, please try again!`);
 } else{
     console.log(userChoice);
 }
