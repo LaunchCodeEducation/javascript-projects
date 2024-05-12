44c212dd67d29bcf47c0b2d7853ee56f5400967c
Felicia <classic_felicia@hotmail.com>
2024-05-11 22:39:56 -0500

    loops practice

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,6 +1,6 @@
 // create an array variable containing the names
 let family = ["Felicia", "Bryan", "Kina", "Luna", "Flower"];
 // write a for loop that prints each name on a different line
-for (let i =){
-
+for (let i = 0; i < family.length; i++){
+    console.log(family[i]);
 }
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,7 +1,7 @@
 // Create a string variable containing your name.
-let name = "Felicia";
+let aName = "Felicia";
 // Write a for loop that prints each character in your name on a different line.
-console.log(name.length);
-for (let i = 0; i < name.length; i++){
-    console.log(name[i]);
+console.log(aName.length);
+for (let i = 0; i < aName.length; i++){
+    console.log(aName[i]);
 }
