c25090898dc8d9457aa7e429bbf88727d5b07dae
nsirtak <nsirtak@gmail.com>
2024-05-13 12:28:20 -0500

    Part One - for Practice

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -4,11 +4,32 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let fCabinet= food.split(",").sort();
+let eCabinet= equipment.split(",").sort();
+let pCabinet= pets.split(",").sort();
+let sCabinet= sleepAids.split(",").sort();
+console.log(`${fCabinet}\n${eCabinet}\n${pCabinet}\n${sCabinet}`);
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-
+let cargoHold = [fCabinet,eCabinet,pCabinet,sCabinet];
+console.log(cargoHold);
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-
+const input = require('readline-sync');
+let userInput = input.question("Which cabinet (0-3)would you like?");
+userInput = Number(userInput);
+console.log(userInput);
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+//If the user entered an invalid number, print an error message.
+if(userInput < cargoHold.length){
+    console.log(cargoHold[userInput]);
+    
+}else{
+    console.log("ERROR!!!!!!!!");
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+let userItem= input.question("which item would you like?");
+if(cargoHold[userInput].includes(userItem)){
+    console.log(`Cabinet ${userInput} DOES contain ${userItem}.`);
+}else {
+    console.log(`Cabinet ${userInput} DOES NOT contain ${userItem}.`);
+}
--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,55 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+  //Exercise #1a. Print the numbers 0 - 20, one number per line
+  for (let i = 0; i <= 20; i++){
+    console.log(i);
+    }
+  //Exercise #1b. Print only the ODD values from 3 - 29, one number per line
+  for (let i = 3; i <= 29; i+=2){
+    console.log(i)
+  }
+  //Exercise #1c. Print the EVEN numbers 12 to -14 in descending order, one number per line.")
+  for (let i = 12; i >= -14; i-=2){
+    console.log(i)
+  }
+  //Exercise #1d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
+    for (let i = 50; i >= 20; i--) {
+        if (i % 3 === 0) {
+          console.log(i);
+        }
+    }
 
-
-
-
-/*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
-
-Construct ``for`` loops to accomplish the following tasks:
+/*Exercise #2: Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+  Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
-
-
-
-
+  let str = "LaunchCode";
+  let arr = [1, 5, "LC101", "blue", 42];
+// Task a: Print each element of the array to a new line
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+//Task b. Print each character of the string - in reverse order - to a new line.
+  for (let i = str.length -1; i >= 0; i--){
+    console.log(str[i]);
+}
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+ 
+  const arr1 = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  const even = [];
+  const odd = [];
+ //a. One array contains the even numbers, and the other holds the odds.
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] % 2 === 0) {
+      even.push(arr1[i]);
+    } else {
+      odd.push(arr1[i]);
+    }
+  }
+  // Print the arrays to confirm the results
+console.log("Even numbers: ", even);
+console.log("Odd numbers: ", odd);
